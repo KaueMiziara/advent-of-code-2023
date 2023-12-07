1798a6d9ed4762a39c195f274d1631bdeb583c36
Kaue Miziara <kauemiziara@gmail.com>
2023-12-07 18:04:22 -0300

    Day 7 part 2 completed

--- Day7/part2.hs
+++ Day7/part2.hs
@@ -52,7 +52,6 @@ formatCards = map charToCard
 parseHand :: (String -> String) -> String -> Hand
 parseHand format input = case words input of
     [hand, bid] -> Hand (format hand) (read bid)
-    _ -> error "Invalid input"
 
 
 parse :: (String -> String) -> String -> [Hand]
