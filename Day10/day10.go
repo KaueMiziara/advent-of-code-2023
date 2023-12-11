08b70ab27e964ba4d25116551a7b6c944d11544d
Kaue Miziara <kauemiziara@gmail.com>
2023-12-10 21:15:20 -0300

    Day 10 part 2 completed

--- Day10/day10.go
+++ Day10/day10.go
@@ -6,6 +6,11 @@ import (
 	"os"
 )
 
+// Input: pipes maze, with symbols representing connections.
+// The maze is a loop.
+// Part1: given a starting point, find the farthest point in the loop. Answer: number of steps to reach it.
+// Part2: find how many tiles are enclosed by the loop.
+
 type Point struct {
 	x int
 	y int
@@ -33,12 +38,57 @@ func main() {
 		}
 	}
 
-	var answer = maxDist
-	fmt.Println("Answer: ", answer)
+	fmt.Println("Part1 answer: ", maxDist)
+
+	countInside := 0
+	for y, row := range grid {
+		for x := range row {
+			if isInside(grid, Point{x, y}, visited) {
+				countInside++
+			}
+		}
+	}
+
+	fmt.Println("Part2 answer: ", countInside)
 }
 
-func findStart(grid []string) Point {
-	for y, row := range grid {
+func isInside(maze []string, p Point, visitedPoints map[Point]int) bool {
+	if _, part := visitedPoints[p]; part {
+		return false
+	}
+
+	count := 0
+	cornerCounts := map[byte]int{}
+
+	for y := p.y + 1; y < len(maze); y++ {
+		check := Point{p.x, y}
+
+		tile := maze[y][p.x]
+
+		if tile == 'S' {
+			tile = findStartTile(Point{p.x, y}, maze)
+		}
+
+		if _, part := visitedPoints[check]; part {
+			if tile == '-' {
+				count++
+			} else if tile != '|' && tile != '.' {
+				cornerCounts[tile]++
+			}
+		}
+	}
+
+	countL, count7 := cornerCounts['L'], cornerCounts['7']
+	countF, countJ := cornerCounts['F'], cornerCounts['J']
+
+	count += max(countL, count7) - abs(countL-count7)
+	count += max(countF, countJ) - abs(countF-countJ)
+
+	return count%2 == 1
+}
+
+func findStart(maze []string) Point {
+	for y, row := range maze {
 		for x, col := range row {
 			if byte(col) == 'S' {
 				return Point{x, y}
@@ -48,10 +98,44 @@ func findStart(grid []string) Point {
 	return Point{}
 }
 
-func nextPoints(grid []string, p Point) []Point {
+func findStartTile(start Point, grid []string) byte {
+	points := nextPoints(grid, start)
+	minX := min(points[0].x, points[1].x)
+	maxX := max(points[0].x, points[1].x)
+	minY := min(points[0].y, points[1].y)
+	maxY := max(points[0].y, points[1].y)
+
+	if points[0].x == points[1].x {
+		return '|'
+	}
+
+	if points[0].y == points[1].y {
+		return '-'
+	}
+
+	if minX < start.x && minY < start.y {
+		return 'J'
+	}
+
+	if maxX > start.x && maxY > start.y {
+		return 'F'
+	}
+
+	if maxX > start.x && minY < start.y {
+		return 'L'
+	}
+
+	if minX < start.x && maxY > start.y {
+		return '7'
+	}
+
+	return '.'
+}
+
+func nextPoints(maze []string, p Point) []Point {
 	points := []Point{}
 
-	switch grid[p.y][p.x] {
+	switch maze[p.y][p.x] {
 	case '|':
 		points = append(points, Point{p.x, p.y + 1})
 		points = append(points, Point{p.x, p.y - 1})
@@ -78,7 +162,10 @@ func nextPoints(grid []string, p Point) []Point {
 
 	case '.':
 	case 'S':
-		down, right, up, left := grid[p.y+1][p.x], grid[p.y][p.x+1], grid[p.y-1][p.x], grid[p.y][p.x-1]
+		down := maze[p.y+1][p.x]
+		right := maze[p.y][p.x+1]
+		up := maze[p.y-1][p.x]
+		left := maze[p.y][p.x-1]
 
 		if down == '|' || down == 'L' || down == 'J' {
 			points = append(points, Point{p.x, p.y + 1})
@@ -114,6 +201,7 @@ func readInput(fileName string) ([]string, error) {
 	defer file.Close()
 
 	var lines []string
+
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		lines = append(lines, scanner.Text())
