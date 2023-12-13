79e9ea950a13d73f99d36a0fbed2363b7856ce88
Kaue Miziara <kauemiziara@gmail.com>
2023-12-13 18:42:43 -0300

    Day 13 completed

--- Day13/day13.py
+++ Day13/day13.py
@@ -2,45 +2,45 @@ import numpy as np
 
 def read_input(file: str) -> list:
     with open(file, 'r') as input_file:
-        input_data = [char for char in input_file.read().split("\n\n")]
+        input = [char for char in input_file.read().split("\n\n")]
 
-    return input_data
+    return input
 
 
 def parse_pattern(pattern):
-    rows = pattern.split("\n")
+    rows = pattern.split('\n')
     rows = [row for row in rows if row]
 
-    return np.array([[el == "#" for el in row] for row in rows])
+    return np.array([[element == '#' for element in row] for row in rows])
 
 
-def find_mirror(pattern, with_smudge=False):
+def find_mirror(pattern, has_smudge=False):
     for row in range(1, pattern.shape[0]):
-        side2 = pattern[row : row + row, :]
-        side1 = pattern[row - side2.shape[0] : row, :]
+        image = pattern[row : 2 * row, :]
+        object = pattern[row - image.shape[0] : row, :]
 
-        if with_smudge:
-            if np.sum(side1 != np.flip(side2, axis=0)) == 1:
+        if has_smudge:
+            if np.sum(object != np.flip(image, axis=0)) == 1:
                 return row
-        elif np.array_equal(side1, np.flip(side2, axis=0)):
+        elif np.array_equal(object, np.flip(image, axis=0)):
             return row
 
     return 0
 
 
 if __name__ == "__main__":
-    input_data = read_input("input.txt")
+    input = read_input("input.txt")
 
-    patterns = list(map(parse_pattern, input_data))
+    patterns = list(map(parse_pattern, input))
 
     row_total = col_total = 0
     for pattern in patterns:
         row_total += find_mirror(pattern)
         col_total += find_mirror(pattern.T)
-    print("Summary of reflection lines:", (100 * row_total + col_total))
+    print("Part 1 answer:", (100 * row_total + col_total))
 
     row_total = col_total = 0
     for pattern in patterns:
         row_total += find_mirror(pattern, True)
         col_total += find_mirror(pattern.T, True)
-    print("Summary of reflection lines (with smudges):", (100 * row_total + col_total))
+    print("Part 2 answer:", (100 * row_total + col_total))
