f25759287c382cd49641dd038e04cccd11050d4f
Kaue Miziara <kauemiziara@gmail.com>
2023-12-15 20:18:22 -0300

    Day 15 part 2 completed

--- Day15/day15.kt
+++ Day15/day15.kt
@@ -3,25 +3,40 @@ package miziara.kaue.aoc.day15
 import java.io.File
 
 fun main() {
-    println("Part1 answer: ${part1()}")
+    println("Part 1 answer: ${AoCDay15().part1()}")
+    println("Part 2 answer: ${AoCDay15().part2()}")
 }
 
-fun part1() = AocHelper().inputData.sumOf { item -> AocHelper().hashAlgorithm(item) }
+class AoCDay15 : AocHelper() {
+    fun part1(): Int = inputData.sumOf { item -> hashAlgorithm(item) }
 
-class AocHelper() {
+    fun part2(): Int = inputData.fold(Array(256) { mutableMapOf<String, Int>() }) { acc, line ->
+        val (value, focalLength) = line.split("=", "-")
+        when {
+            "-" in line -> acc[hashAlgorithm(value)] -= value
+            else -> acc[hashAlgorithm(value)][value] = focalLength.toInt()
+        }
+        acc
+    }.withIndex().sumOf { (i, map) ->
+        (i + 1) * map.values.withIndex().sumOf { (j, value) -> (j + 1) * value }
+    }
+}
+
+open class AocHelper() {
     fun hashAlgorithm(input: String): Int {
         var result = 0
 
         for (char in input) {
-            if (char != '\n' && char != ' ')
-            result += char.code
-            result *= 17
-            result %= 256
+            if (char != '\n' && char != ' ') {
+                result += char.code
+                result *= 17
+                result %= 256
+            }
         }
 
         return result
     }
 
     private val inputFile = File("input.txt").readText()
-    val inputData = inputFile.split(",")
+    val inputData: List<String> = inputFile.split(",")
 }
